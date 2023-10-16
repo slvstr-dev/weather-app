124db3de296fe5d7daa1c9e451d2545d6eec820e
Sylvester <hello@slvstr.dev>
2023-10-16 20:49:07 +0200

    Add initial FilterModal

--- app/(modal)/filter.tsx
+++ app/(modal)/filter.tsx
@@ -1,9 +1,33 @@
-import { View, Text } from 'react-native';
+import { useNavigation } from 'expo-router';
+import { View, Text, FlatList, ListRenderItem } from 'react-native';
 
-export default function filter() {
+import data from '@/assets/data/filter.json';
+import { Button } from '@/components/ui/Button/Button';
+
+interface Category {
+  name: string;
+  count: number;
+  checked?: boolean;
+}
+
+export default function FilterModal() {
+  const { goBack } = useNavigation();
+
+  const renderItem: ListRenderItem<Category> = ({ item }) => {
+    return (
+      <View>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>filter</Text>
+    <View className="bg-lightGrey flex-1 p-6">
+      <FlatList data={data} renderItem={renderItem} />
+
+      <View className="absolute bottom-0 left-0 right-0 h-[100px] bg-white p-2.5 shadow-md">
+        <Button onPress={goBack}>Done</Button>
+      </View>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,13 @@
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-import { Stack } from 'expo-router';
+import { Stack, useNavigation } from 'expo-router';
+import { TouchableOpacity } from 'react-native';
+import resolveConfig from 'tailwindcss/resolveConfig';
 
 import Header from '@/components/layout/Header/Header';
+import { Icon } from '@/components/ui/Icon/Icon';
+import tailwindConfig from '@/tailwind.config';
+
+const fullConfig = resolveConfig(tailwindConfig);
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -9,6 +15,8 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
+  const { goBack } = useNavigation();
+
   return (
     <BottomSheetModalProvider>
       <Stack>
@@ -19,7 +27,22 @@ export default function RootLayout() {
           }}
         />
 
-        <Stack.Screen name="(modal)/filter" />
+        <Stack.Screen
+          name="(modal)/filter"
+          options={{
+            presentation: 'modal',
+            headerTitle: 'Filter',
+            headerShadowVisible: false,
+            headerStyle: {
+              backgroundColor: fullConfig.theme?.colors?.lightGrey as string,
+            },
+            headerLeft: () => (
+              <TouchableOpacity onPress={goBack}>
+                <Icon icon="CloseOutline" size={28} className="text-primary" />
+              </TouchableOpacity>
+            ),
+          }}
+        />
       </Stack>
     </BottomSheetModalProvider>
   );
