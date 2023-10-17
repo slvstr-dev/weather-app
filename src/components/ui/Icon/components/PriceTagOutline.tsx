211f2ceef6244772e55312dc201ac62f8e1a8816
Sylvester <hello@slvstr.dev>
2023-10-17 17:40:30 +0200

    Add more icons

---
+++ src/components/ui/Icon/components/PriceTagOutline.tsx
@@ -0,0 +1,18 @@
+import Svg, { SvgProps, Path } from 'react-native-svg';
+
+export const PriceTagOutline = (props: SvgProps) => {
+  return (
+    <Svg viewBox="0 0 512 512" {...props}>
+      <Path
+        d="M435.25 48h-122.9a14.46 14.46 0 0 0-10.2 4.2L56.45 297.9a28.85 28.85 0 0 0 0 40.7l117 117a28.85 28.85 0 0 0 40.7 0L459.75 210a14.46 14.46 0 0 0 4.2-10.2v-123a28.66 28.66 0 0 0-28.7-28.8Z"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={48}
+      />
+
+      <Path d="M384 160a32 32 0 1 1 32-32 32 32 0 0 1-32 32Z" fill="currentColor" />
+    </Svg>
+  );
+};
