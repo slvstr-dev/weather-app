707e1c85cf909323b09b8cb14ee9fde86c3dd529
Sylvester <hello@slvstr.dev>
2023-10-17 17:48:59 +0200

    Add renderItem for performance reasons

--- app/(modal)/filter.tsx
+++ app/(modal)/filter.tsx
@@ -1,21 +1,23 @@
 import { useNavigation } from 'expo-router';
-import { View, FlatList } from 'react-native';
+import { useCallback } from 'react';
+import { View, FlatList, ListRenderItem } from 'react-native';
 
 import data from '@/assets/data/filter.json';
 import { Button } from '@/components/ui/Button/Button';
-import { FilterItem } from '@/components/ui/FilterItem/FilterItem';
+import { FilterItem, FilterItemProps } from '@/components/ui/FilterItem/FilterItem';
 import { FilterOptions } from '@/components/ui/FilterOptions/FilterOptions';
 
 export default function FilterModal() {
   const { goBack } = useNavigation();
 
+  const renderItem: ListRenderItem<FilterItemProps> = useCallback(
+    ({ item }) => <FilterItem {...item} />,
+    [],
+  );
+
   return (
     <View className="bg-lightGrey flex-1 p-6">
-      <FlatList
-        data={data}
-        renderItem={({ item }) => <FilterItem {...item} />}
-        ListHeaderComponent={<FilterOptions />}
-      />
+      <FlatList data={data} renderItem={renderItem} ListHeaderComponent={<FilterOptions />} />
 
       <View className="absolute bottom-0 left-0 right-0 h-[100px] bg-white p-2.5 shadow-md">
         <Button onPress={goBack}>Done</Button>
