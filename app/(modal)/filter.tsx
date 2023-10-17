c3a473749ddc8587d01276b4092d33ac489281ff
Sylvester <hello@slvstr.dev>
2023-10-17 17:41:22 +0200

    Add FilterItem

--- app/(modal)/filter.tsx
+++ app/(modal)/filter.tsx
@@ -1,29 +1,16 @@
 import { useNavigation } from 'expo-router';
-import { View, Text, FlatList, ListRenderItem } from 'react-native';
+import { View, FlatList } from 'react-native';
 
 import data from '@/assets/data/filter.json';
 import { Button } from '@/components/ui/Button/Button';
-
-interface Category {
-  name: string;
-  count: number;
-  checked?: boolean;
-}
+import { FilterItem } from '@/components/ui/FilterItem/FilterItem';
 
 export default function FilterModal() {
   const { goBack } = useNavigation();
 
-  const renderItem: ListRenderItem<Category> = ({ item }) => {
-    return (
-      <View>
-        <Text>{item.name}</Text>
-      </View>
-    );
-  };
-
   return (
     <View className="bg-lightGrey flex-1 p-6">
-      <FlatList data={data} renderItem={renderItem} />
+      <FlatList data={data} renderItem={({ item }) => <FilterItem {...item} />} />
 
       <View className="absolute bottom-0 left-0 right-0 h-[100px] bg-white p-2.5 shadow-md">
         <Button onPress={goBack}>Done</Button>
