2a4e92bcafca3254d102f84d5c913692fb179f4f
Sylvester <hello@slvstr.dev>
2023-10-19 20:55:28 +0200

    Add Categories & Restaurants

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,23 @@
-import { View, Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { Categories } from '@/components/ui/Categories/Categories';
+import { Restaurants } from '@/components/ui/Restaurants/Restaurants';
 
 export default function IndexPage() {
   return (
-    <View>
-      <Text>IndexPage</Text>
-    </View>
+    <SafeAreaView className="mt-[60px] bg-lightGrey flex-1" edges={['bottom', 'top']}>
+      <ScrollView>
+        <Categories />
+
+        <Text className="text-lg font-bold mt-2 px-4">Top picks in you neighbourhood</Text>
+
+        <Restaurants />
+
+        <Text className="text-lg font-bold mt-2 px-4">Offers near you</Text>
+
+        <Restaurants />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
